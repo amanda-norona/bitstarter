a1c4fc1da4532392b63332bc9ce7a76f36ebfe6e
Ubuntu <ubuntu@ip-172-31-43-16.us-west-2.compute.internal>
2014-09-28 16:27:32 +0000

    index.js to 8080

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var express = require('express')
-var app = express();
+//var app = express();
 
 var fs = require('fs');
 var data =fs.readFileSync("index.html", "utf-8");
 
-//var app =express.createServer(express.logger());
+var app =express.createServer(express.logger());
 
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
+//app.set('port', (process.env.PORT || 8080))
+//app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send(data);
 });
-//var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 
-app.listen(app.get('port'), function() {
+app.listen('port', function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
